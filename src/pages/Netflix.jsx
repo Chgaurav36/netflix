562a7c77751b3382beda0fcc1cd00e09258bc464
Chgaurav36 <gauravchaurasia619@gmail.com>
2024-01-17 12:43:22 +0530

    fetching movie data from tmdb api

--- src/pages/Netflix.jsx
+++ src/pages/Netflix.jsx
@@ -1,19 +1,38 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar';
 import backgroundimage from "../assets/home.jpg"
 import MovieLogo from "../assets/homeTitle.webp"
 import { FaPlay } from "react-icons/fa"
 import { AiOutlineInfoCircle } from "react-icons/ai"
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMovies, getGenres } from '../store';
+import Slider from '../components/Slider';
 
 export default function Netflix() {
 
   const [isScrolled, setIsScrolled] = useState(false);
+  const navigate = useNavigate();
+  const genresLoaded = useSelector((state) => state.netflix.genresLoaded);
+  const movies = useSelector((state) => state.netflix.movies);
+  const dispatch = useDispatch();
+
+useEffect(()=>{
+  dispatch(getGenres());
+}, []);
+
+useEffect(() =>{ 
+  if (genresLoaded) dispatch(fetchMovies({type: "all"}));
+}, []);
+
 
   window.onscroll = () => {
     setIsScrolled(window.pageYOffset === 0 ? false: true);
     return ()=> (window.onscroll = null);
   };
+
+
   return (
     <Container>
       <Navbar isScrolled={isScrolled}/>
@@ -24,11 +43,12 @@ export default function Netflix() {
           <img src={MovieLogo} alt="MovieLogo" />
         </div>
         <div className="buttons flex">
-          <button className='flex j-center a-center'><FaPlay/>Play</button>
+          <button className='flex j-center a-center' onClick={() =>navigate('/player')}><FaPlay/>Play</button>
           <button className='flex j-center a-center'><AiOutlineInfoCircle/>More info</button>
         </div>
       </div>
       </div>
+      <Slider movies ={movies}/>
     </Container>
   )
 };
---
+++ src/utils/constants.jsx
@@ -0,0 +1,2 @@
+export const API_KEY = "8ba98a4a9548201ca8eab1740a2c3e32";
+export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
