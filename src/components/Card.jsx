cd59b5cb3341c8dbbdb89c09d5adc27cb7b4f098
Chgaurav36 <gauravchaurasia619@gmail.com>
2024-01-17 17:40:59 +0530

    committing till where i have completed to start further

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import video from "../assets/trailer.mp4";
+import {IoPlayCircleSharp} from "react-icons/io5";
+import {RiThumbUpFill, RiThumbDownFill} from "react-icons/ri";
+import {BsCheck} from "react-icons/bs";
+import {AiOutlinePlus} from "react-icons/ai";
+import {BiChevronDown} from "react-icons/bi";
 
-export default function Card() {
-  return (
-    <div>
-      Card
-    </div>
-  )
+ function Card({movieData, isLiked= false}) {
+  
+  const [isHovered, setIsHovered] = useState(false);
+  const navigate = useNavigate();
+  return <Container 
+  onMouseEnter={() =>{setIsHovered(true)}}
+  onMouseLeave={() =>{setIsHovered(false)}}
+  >
+    <img 
+       src={`https://image.tmdb.org/t/p/w500${movieData.image}`}
+       alt="movie" />
+       {
+        isHovered && (
+            <div className="hover">
+                <div className="image-video-container">
+                    <img 
+                    src={`https://image.tmdb.org/t/p/w500${movieData.image}`} 
+                    alt="movie"
+                    onClick={() => navigate("/player")} />
+                    <video 
+                    src={video} 
+                    autoPlay 
+                    loop 
+                    muted 
+                    onClick={() => navigate("/player")}
+                    />
+                </div>
+                <div className="info-container flex column">
+                    <h3 className='name' onClick={() => navigate("/player")}>
+                    {movieData.name}
+                    </h3>
+                    <div className="icons flex j-between">
+                        <div className="controls flex">
+                            <IoPlayCircleSharp title='play'
+                                onClick={() => navigate("/player")}
+                            />
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )}
+    </Container>
+  
 }
+
+const Container = styled.div`
+`;
+
+
+export default Card;
